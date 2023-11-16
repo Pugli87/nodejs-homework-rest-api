d022d1b03a25ca701adf35338416348dc037ce50
Pugli87 <119467014+Pugli87@users.noreply.github.com>
2023-11-16 02:12:01 -0500

    Update getById.js

--- controllers/contacts/getById.js
+++ controllers/contacts/getById.js
@@ -5,6 +5,12 @@ const getContactById = async (req, res, next) => {
   const { id } = req.params;
   const contact = await Contact.findById(id);
 
+  if (!contact) {
+    return res.status(404).json({
+      message: 'Not found',
+    });
+  }
+
   res.status(200).json(contact);
 };
 
