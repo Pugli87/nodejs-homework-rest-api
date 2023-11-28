adeae9852c1c17471c1249e7903fdd27cec53050
Pugli87 <119467014+Pugli87@users.noreply.github.com>
2023-11-28 04:52:21 -0500

    delete comments and add models user in index.js

--- models/contact.js
+++ models/contact.js
@@ -1,32 +1,3 @@
-/*const { Schema, model } = require('mongoose');
-
-const contactSchema = Schema(
-  {
-    name: {
-      type: String,
-      required: true,
-    },
-    email: {
-      type: String,
-    },
-    phone: {
-      type: String,
-    },
-    favorite: {
-      type: Boolean,
-      default: false,
-    },
-  },
-  {
-    versionKey: false,
-    timestamps: true,
-  },
-);
-
-const Contact = model('contacts', contactSchema);
-
-module.exports = Contact;*/
-
 const Joi = require('joi');
 const { Schema, model } = require('mongoose');
 
